99645a7cc4670247a5ce928c33ccff6c7f02d52d
KRRISH-GAMING <79586177+KRRISH-GAMING@users.noreply.github.com>
2025-10-26 11:13:47 +0530

    Add files via upload

--- plugins/start.py
+++ plugins/start.py
@@ -39,7 +39,7 @@ async def start(client, message):
         buttons = [
             [InlineKeyboardButton("🌟 Our Premium Plans", callback_data="x1")],
             #[InlineKeyboardButton("📊 Check Your Subscription", callback_data="x2")],
-            [InlineKeyboardButton("♈ How To Buy", url="https://t.me/Open_Shorten_Link_Tutorial/13")],
+            [InlineKeyboardButton("♈ How To Buy Premium", url="https://t.me/Open_Shorten_Link_Tutorial/13")],
             [InlineKeyboardButton("🆘 Help & Support", callback_data="x3")]
         ]
 
@@ -110,7 +110,7 @@ async def callback(client, query):
             buttons = [
                 [InlineKeyboardButton("🌟 Our Premium Plans", callback_data="x1")],
                 #[InlineKeyboardButton("📊 Check Your Subscription", callback_data="x2")],
-                [InlineKeyboardButton("♈ How To Buy", url="https://t.me/Open_Shorten_Link_Tutorial/13")],
+                [InlineKeyboardButton("♈ How To Buy Premium", url="https://t.me/Open_Shorten_Link_Tutorial/13")],
                 [InlineKeyboardButton("🆘 Help & Support", callback_data="x3")]
             ]
             await safe_action(
